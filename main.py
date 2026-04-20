0067bd95def9e0d50b20f733fd255b069f62d5fd
Salohiddin16 <musurmonovsalohiddin0@gmail.com>
2026-04-20 16:39:59 +0300

    ozgartirildi

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 print(2+2)
 
+print("Hello git hub")
 
 """
 git init
-git add .
+git add README.md
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/Salohiddin16/github-project.git
 git push -u origin main
-
 """
