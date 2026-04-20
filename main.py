cfca6e67438764e27bf7cbe5c1a3f75ca4eb76e0
Salohiddin16 <musurmonovsalohiddin0@gmail.com>
2026-04-20 16:32:42 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print(2+2)
+
+"""
+echo "# github-project" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Salohiddin16/github-project.git
+git push -u origin main
+
+"""
